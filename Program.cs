71053ddf14de2eaeba01c30de20aa0295a9cd519
MiaDobler <113926918+MiaDobler@users.noreply.github.com>
2023-09-14 23:14:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace CSCI2910_Lab1
+{
+
+    //DO ERROR HANDLING
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            List<VideoGame> videogames = new List<VideoGame>();
+
+            string projectRootFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.ToString();
+            string filePath = projectRootFolder + "/videogames.csv";
+            
+            using (var sr = new StreamReader(filePath)) //from Will's code example
+            {
+                sr.ReadLine(); 
+
+                while (!sr.EndOfStream)  
+                {
+                    string line = sr.ReadLine();
+
+                    string[] lineData = line.Split(',');
+
+                    //data is Name, Platform, Year, Genre, Publisher, NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales order based on header row
+                    videogames.Add(new VideoGame(lineData[0], lineData[1], Convert.ToInt16(lineData[2]), lineData[3], lineData[4], Convert.ToDouble(lineData[5]), Convert.ToDouble(lineData[6]), Convert.ToDouble(lineData[7]), Convert.ToDouble(lineData[8]), Convert.ToDouble(lineData[9])));
+                }
+            }
+            videogames.Sort();
+
+
+
+            // ----------------- METHODS ----------------- //
+
+            //publisher data user thing
+            Console.Write("\nPLease enter a publisher: ");
+            string userPublisher = Console.ReadLine();
+
+            PublisherData(userPublisher, videogames);
+
+            void PublisherData(string userPublisher, List<VideoGame> games2)
+            {                  
+                    IEnumerable<string> publishers = games2.Select(x => x.Publisher);
+                    List<string> publishers2 = publishers.Distinct().ToList();
+
+                    IEnumerable<VideoGame> publisherCompared = new List<VideoGame>();
+                    publisherCompared = games2.Where(game  => game.Publisher == userPublisher);
+
+                    foreach (var item in publisherCompared)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                double publisherPercentageCalc = (Convert.ToDouble(publisherCompared.Count()) / Convert.ToDouble(games2.Count()) * 100);
+                double publisherPercentage = Math.Round(publisherPercentageCalc, 2);
+
+                Console.Write($"\nOut of {games2.Count()} games, {publisherCompared.Count()} are devloped by {userPublisher}, which is {publisherPercentage}%.\n");
+            }
+
+
+
+            //genre data user thing
+
+            Console.Write("\nPLease enter a genre: ");
+            string userGenre = Console.ReadLine();
+
+            GenreData(userGenre, videogames);
+
+            void GenreData(string userGenre, List<VideoGame> games3)
+            {
+                IEnumerable<string> genres = games3.Select(x => x.Genre);
+                List<string> genres2 = genres.Distinct().ToList();
+
+                IEnumerable<VideoGame> genreCompared = new List<VideoGame>();
+                genreCompared = games3.Where(game => game.Genre == userGenre);
+
+                foreach (var item in genreCompared)
+                {
+                    Console.WriteLine(item);
+                }
+
+                double genrePercentageCalc = (Convert.ToDouble(genreCompared.Count()) / Convert.ToDouble(games3.Count()) * 100);
+                double genrePercentage = Math.Round(genrePercentageCalc, 2);
+
+                Console.Write($"\nOut of {games3.Count()} games, {genreCompared.Count()} are {userGenre} games, which is {genrePercentage}%.\n");
+            }
+
+        }        
+
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI2910_Lab1
+{
+    public class VideoGame :IComparable<VideoGame>
+    {
+        public string Name {  get; set; }
+        public string Platform { get; set; }
+
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NA_Sales { get; set; }
+        public double EU_Sales { get; set; }
+        public double JP_Sales { get; set; }
+        public double Other_Sales { get; set; }
+        public double Global_Sales { get; set; }
+
+        public VideoGame() 
+        { 
+            this.Name = Name;
+            this.Platform = Platform;
+            this.Year = Year;
+            this.Genre = Genre;
+            this.Publisher = Publisher;
+            this.NA_Sales = NA_Sales;
+            this.EU_Sales = EU_Sales;
+            this.JP_Sales = JP_Sales;
+            this.Other_Sales = Other_Sales;
+            this.Global_Sales = Global_Sales;
+        }
+
+        public VideoGame(string name, string platform, int year, string genre, string publisher, double nA_Sales, double eU_Sales, double jP_Sales, double other_Sales, double global_Sales)
+        {
+            Name = name;
+            Platform = platform;
+            Year = year;
+            Genre = genre;
+            Publisher = publisher;
+            NA_Sales = nA_Sales;
+            EU_Sales = eU_Sales;
+            JP_Sales = jP_Sales;
+            Other_Sales = other_Sales;
+            Global_Sales = global_Sales;
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+            msg += $"Name:\t\t\t {Name}\n";
+            msg += $"Platform:\t\t {Platform}\n";
+            msg += $"Year:\t\t\t {Year}\n";
+            msg += $"Genre:\t\t\t {Genre}\n";
+            msg += $"Publisher:\t\t {Publisher}\n";
+            msg += $"NA Sales:\t\t {NA_Sales}\n";
+            msg += $"EU Sales:\t\t {EU_Sales}\n";
+            msg += $"JP Sales:\t\t {JP_Sales}\n";
+            msg += $"Other Sales:\t\t {Other_Sales}\n";
+            msg += $"Global Sales:\t\t {Global_Sales}\n";
+            msg += "------------------------------\n";
+           
+            return msg;
+        }
+
+        public int CompareTo(VideoGame obj) //Method help from Zach
+        { 
+            if(obj == null)
+                return 1;
+            return Comparer<string>.Default.Compare(Name, obj.Name);
+        }
+    }
+}
